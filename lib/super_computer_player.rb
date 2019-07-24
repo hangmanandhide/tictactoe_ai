d4c03d1a522b053e0005243b98fcb262635b1ad6
hangmanandhide <9014584+hangmanandhide@users.noreply.github.com>
2019-07-23 23:34:02 -0700

    Created SupercomputerPlayer with redefined unbeatable move method.

--- lib/super_computer_player.rb
+++ lib/super_computer_player.rb
@@ -2,6 +2,29 @@ require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+    node = TicTacToeNode.new(game.board, mark)
+    possible_moves = node.children
+
+    a_winning_move = nil
+
+    possible_moves.each do |child_node|
+      if child_node.winning_node?(mark)
+        a_winning_move  = child_node
+        break
+      end
+    end
+
+    return a_winning_move.prev_move_pos if a_winning_move
+
+    possible_moves.each do |child_node|
+      if !child_node.losing_node?(mark) 
+        a_winning_move  = child_node
+        break
+      end
+    end
+
+    return a_winning_move.prev_move_pos if a_winning_move
+
   end
 end
 
