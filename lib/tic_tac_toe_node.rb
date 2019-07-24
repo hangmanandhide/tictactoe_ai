9e43b024e07ee048e5329ebf0a98418a3a3b4b3a
hangmanandhide <9014584+hangmanandhide@users.noreply.github.com>
2019-07-23 22:42:26 -0700

    Created winning_node?

--- lib/tic_tac_toe_node.rb
+++ lib/tic_tac_toe_node.rb
@@ -8,9 +8,11 @@ class TicTacToeNode
   end
 
   def losing_node?(evaluator)
+    return board.over? && 
   end
 
   def winning_node?(evaluator)
+    board.over? == evaluator
   end
 
   # This method generates an array of all moves that can be made after
@@ -18,14 +20,21 @@ class TicTacToeNode
   #To create this method, it will be necessary to iterate through all positions that are empty? on the board object. For each empty position, create a node by duping the board and putting a next_mover_mark in the position.
   def children
     children_array = []
-    
+
     (0..2).each do |row|
       (0..2).each do |col|
         pos = [row, col]
-        next unless @board[pos].empty?
+        next unless @board.empty?(pos)
+
+        duped_board = board.dup
+        duped_board[pos] = self.next_mover_mark
+        next_mover_mark = (self.next_mover_mark == :x ? :o : :x)
+
+        children << TicTacToeNode.new(duped_board, next_mover_mark, pos)
       end
     end
 
+    children
 
   end
 end
